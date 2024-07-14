609f6ba652c0fc59642bec07884d3fcecbc94bcb
BestTTTA <bst12546@gmail.com>
2024-07-14 13:38:24 +0700

    check

--- helloworld/main.go
+++ helloworld/main.go
@@ -40,16 +40,16 @@ func main() {
 		fmt.Println("Invalid")
 	}
 
-	// switch {
-	// case value == 1:
-	// 	fmt.Println("Hello")
-	// case value == 2:
-	// 	fmt.Println("Bonjour")
-	// case value == 3:
-	// 	fmt.Println("Namstay")
-	// default:
-	// 	fmt.Println("Invalid")
-	// }
+	switch {
+	case value == 1:
+		fmt.Println("Hello")
+	case value == 2:
+		fmt.Println("Bonjour")
+	case value == 3:
+		fmt.Println("Namstay")
+	default:
+		fmt.Println("Invalid")
+	}
 
 }
 
